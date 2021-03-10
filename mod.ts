be886e592030559f692ae45b587306cdc4a7ed55
MierenManz <christiaanmens@gmail.com>
2021-03-10 11:29:42 +0100

    deno fmt

--- mod.ts
+++ mod.ts
@@ -2,7 +2,7 @@ import { displayPicture } from "./src/console.ts";
 const imageLocation = Deno.args[0];
 const imageURL = new URL(imageLocation, import.meta.url);
 let image: Uint8Array;
-if (imageURL.protocol !== "file:") image = new Uint8Array(await (await fetch(imageURL)).arrayBuffer());
-else image = await Deno.readFile(imageURL);
-console.log(Deno.consoleSize(Deno.stdout.rid));
+if (imageURL.protocol !== "file:") {
+  image = new Uint8Array(await (await fetch(imageURL)).arrayBuffer());
+} else image = await Deno.readFile(imageURL);
 displayPicture(image);
--- src/parsepic.ts
+++ src/parsepic.ts
@@ -11,15 +11,24 @@ export async function* parsePic(rawData: Uint8Array): AsyncGenerator<string> {
     const { image, width } = decode(rawData);
     raw = image;
     imageWidth = width;
-    if (Deno.consoleSize(Deno.stdout.rid).columns / 2 < width) throw "Image is too big";
+    if (
+      Deno.consoleSize(Deno.stdout.rid).columns * 0.5 < width
+    ) {
+      throw "Image is too big";
+    }
   } else throw "Unsupported image format";
+  let buff: string[] = [];
   for (let i = 0; i < raw.length; i += 4) {
     if ((i * 0.25 % imageWidth === 0) && i !== 0) {
-      yield "\u001b[0m\n";
+      buff.push("\u001b[0m\n");
+      yield buff.join("");
+      buff = [];
     }
-    yield `\u001b[48;2;${raw[i]};${raw[i+1]};${raw[i+2]}m  `;
+    if (raw[i + 3] === 0) buff.push("\u001b[0m  ");
+    else buff.push(`\u001b[48;2;${raw[i]};${raw[i + 1]};${raw[i + 2]}m  `);
   }
-  yield "\u001b[0m";
+  buff.push("\u001b[0m");
+  yield buff.join("");
   return;
 }
 
@@ -27,7 +36,7 @@ function getFileType(rawFile: Uint8Array): string {
   const signatures: Signatures = {
     "png": [
       [137, 80, 78, 71, 13, 10, 26, 10, 0, 0, 0, 13, 73, 72, 68, 82],
-    ]
+    ],
   };
 
   for (const fileType in signatures) {
